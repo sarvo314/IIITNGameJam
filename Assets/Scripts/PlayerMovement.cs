eb34f2d2c1a201674df73c0aa79afdd2e5e531b3
Sarvottam <sarvottamswaroop100@gmail.com>
2023-09-22 17:11:57 +0530

    Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4f;
+    private float jumpForce = 10f;
+    private float sprintModifier = 1.5f;
+    private float gravity = -9.81f;
+    private Rigidbody rb;
+    private bool isGrounded;
+    [SerializeField]
+    private CharacterController controller;
+    private Vector3 velocity;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        velocity = Vector3.zero;
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+        //Get the player's input
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // Check if the player is sprinting
+        bool sprinting = Input.GetKey(KeyCode.LeftShift);
+
+        // Calculate the player's movement direction
+        //Vector3 direction = new Vector3(x, 0, y);
+        Vector3 direction = transform.right * x + transform.forward * z;
+        direction.Normalize();
+
+        // Apply the player's movement speed
+        float adjustedSpeed = speed;
+        if (sprinting)
+        {
+            adjustedSpeed *= sprintModifier;
+        }
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y += jumpForce * Time.deltaTime;
+        }
+
+        controller.Move(direction * adjustedSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        // Check if the player is grounded
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f))
+        {
+            isGrounded = true;
+            velocity.y = 0;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
