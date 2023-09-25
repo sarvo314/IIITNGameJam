77dd459a192f84c50837dd10de817e5929a10b39
Sarvottam <sarvottamswaroop100@gmail.com>
2023-09-25 23:55:24 +0530

    Player controller working ADded punching animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,34 @@ public class Player : MonoBehaviour, IDamageable
     private float MAX_HEALTH = 100;
     private float MIN_HEALTH = 0;
 
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float punchRaycast;
+
+    string[] combatMove = { "Punch", "PunchToElbow", "EightPunch" };
     void Awake()
     {
         health = MAX_HEALTH;
     }
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            int move = Random.Range(0, combatMove.Length);
+            animator.SetTrigger(combatMove[move]);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.forward, out hit, punchRaycast))
+            {
+                if (hit.collider.gameObject.CompareTag("Enemy"))
+                {
+                    Debug.Log("We hit enemy");
+                }
+            }
+        }
+    }
     public void Heal(float heal)
     {
         health = Mathf.Min(MAX_HEALTH, health + heal);
